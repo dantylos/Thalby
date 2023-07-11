867808300173b914127d071e8c6d0f566ead1d20
dantylos <danihme89@gmail.com>
2023-07-11 18:41:07 +0300

    carusels

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:true,
+  navText: [ '', ' ' ],
+  autoplay:true,
+  autoplayTimeout:2000,
+  responsive:{
+      0:{
+          items:2
+      },
+
+      1000:{
+          items:3
+      }
+  }
+});
+$( function() {
+  $( "#dialog" ).dialog({
+    autoOpen: false,
+    show: {
+      effect: "blind",
+      duration: 1000
+    },
+    hide: {
+      effect: "explode",
+      duration: 1000
+    }
+  });
+
+  $( "#opener" ).on( "click", function() {
+    $( "#dialog" ).dialog( "open" );
+  });
+} );
